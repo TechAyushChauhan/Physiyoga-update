d3d632001ef9d18edb5425e71e01f7cf32509281
Ayush Chauhan <mynameisayuh007@gmial.com>
2024-12-07 15:32:21 +0530

    added env

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -8,6 +8,7 @@ export async function connectToDatabase(): Promise<MongoDBConnection> {
     const uri = process.env.MONGODB_URI;
     const dbName = process.env.MONGODB_DB;
 
+    // Check if MONGODB_URI and MONGODB_DB are defined in environment variables
     if (!uri) {
         throw new Error('Please define the MONGODB_URI environment variable in .env.local');
     }
@@ -16,14 +17,19 @@ export async function connectToDatabase(): Promise<MongoDBConnection> {
         throw new Error('Please define the MONGODB_DB environment variable in .env.local');
     }
 
+    // If cached client and database are available, return them
     if (cachedClient && cachedDb) {
         return { client: cachedClient, db: cachedDb };
     }
 
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    // Create a new MongoClient instance without deprecated options
+    const client = new MongoClient(uri);
+
+    // Connect to the database
     await client.connect();
     const db = client.db(dbName);
 
+    // Cache the client and database for future use
     cachedClient = client;
     cachedDb = db;
 
