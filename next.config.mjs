c9fd1ba353c6ce4138739be87a75f6d17499969a
Ayush Chauhan <mynameisayuh007@gmial.com>
2024-12-16 16:35:50 +0530

    added style

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,7 @@ const nextConfig = {
   images: {
     domains: ['png.pngtree.com', 'a.storyblok.com'], // Allow images from both external domains
   },
-  experimental: {
-    // Optional: Enable appDir or any other experimental features you need
-    appDir: true,
-  },
-  webpack(config) {
-    // Optional: Customize webpack configurations, for example adding support for SVG or other loaders
-    return config;
-  },
+  
 };
 
 export default nextConfig;
