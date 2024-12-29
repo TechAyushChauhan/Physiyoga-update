d59a3e54e31fd87b406e39f215575418b34457be
Ayush Chauhan <mynameisayuh007@gmial.com>
2024-12-29 13:25:20 +0530

    added ref

--- src/app/Components/Loader/index.tsx
+++ src/app/Components/Loader/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
 import { ProgressSpinner } from 'primereact/progressspinner';
 import { useState, useRef, useEffect } from 'react';
